03989e5692f0fd3430d1803d44af2eff295328c5
github username <github email address>
2019-06-08 17:58:04 -0400

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,15 @@
+def speak_to_grandma(greeting)
+    if greeting == "I LOVE YOU GRANDMA!"
+        return "I LOVE YOU TOO PUMPKIN!"
+    elsif greeting == greeting.upcase
+        return "NO, NOT SINCE 1938!"
+    else 
+        return "HUH?! SPEAK UP, SONNY!"
+    end
+end
+
+
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
@@ -7,7 +19,13 @@
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 
-# NO, NOT SINCE 1938!
+# 
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+
+        
+
+
+
